09c95a8c2a8b1f8d26859c43f1766a78cf023b10
Mikhail Pushkin <lukashevichmikhail@gmail.com>
2017-06-21 16:30:29 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MyTableComponent } from './my-table/my-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
